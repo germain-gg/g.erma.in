f3ca075a5b0399a61201dddf4d67b993425c85a4
Germain <germain@grabyo.com>
2018-11-05 12:09:28 +0000

    extract head tags to its own component

---
+++ src/components/head.js
@@ -0,0 +1,25 @@
+import Helmet from 'react-helmet'
+
+export const Head = ({ title, description, url, name }) => {
+	<Helmet>
+		<title>{title}</title>
+		<meta name="description" content={description}/>
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+		<meta property="og:type" content="website"/>
+		<meta property="og:url" content={url} />
+		<meta property="og:title" content={title}/>
+		<meta property="og:image" content=""/>
+		<meta property="og:description" content={description}/>
+		<meta property="og:site_name" content={name} />
+		<meta property="og:locale" content="en_GB"/>
+		<meta name="twitter:card" content="summary"/>
+		<meta name="twitter:creator" content="@germainsouquet"/>
+		<meta name="twitter:url" content={url}/>
+		<meta name="twitter:title" content={name} />
+		<meta name="twitter:description" content={description}/>
+
+		<html lang="en" dir="ltr"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    </Helmet>
+};
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
 
+import Head from '../components/head';
 import Header from '../components/header';
 import Main from '../components/main';
 
@@ -10,28 +10,14 @@ import './core.css'
 
 const Layout = ({ children, data }) => (
   <div>
-    <Helmet>
-			<title>{data.site.siteMetadata.title}</title>
-			<meta name="description" content={data.site.siteMetadata.description}/>
-			<meta name="apple-mobile-web-app-capable" content="yes" />
-			<meta name="apple-mobile-web-app-status-bar-style" content="black"/>
-			<meta property="og:type" content="website"/>
-			<meta property="og:url" content={data.site.siteMetadata.url} />
-			<meta property="og:title" content={data.site.siteMetadata.title}/>
-			<meta property="og:image" content=""/>
-			<meta property="og:description" content={data.site.siteMetadata.description}/>
-			<meta property="og:site_name" content={data.site.siteMetadata.name} />
-			<meta property="og:locale" content="en_GB"/>
-			<meta name="twitter:card" content="summary"/>
-			<meta name="twitter:creator" content="@germainsouquet"/>
-			<meta name="twitter:url" content={data.site.siteMetadata.url}/>
-			<meta name="twitter:title" content={data.site.siteMetadata.name} />
-			<meta name="twitter:description" content={data.site.siteMetadata.description}/>
 
-			<html lang="en" dir="ltr"/>
-			<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-    </Helmet>
-		
+		<Head
+			url={data.site.siteMetadata.url}
+			name={data.site.siteMetadata.name}
+			title={data.site.siteMetadata.title}
+			description={data.site.siteMetadata.description}
+		/>
+
 		<Header
 			siteTitle={data.site.siteMetadata.headerName}
 		/>
