dc682b5eb3dce992105f67148d2544a010c35ebe
Germain Souquet <germain@souquet.com>
2018-11-05 20:02:04 +0000

    :rocket: add SSR support

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
- // You can delete this file if you're not using it
+const { renderToString } = require("react-dom/server");
+const { ServerStyleSheet } = require("styled-components");
+
+exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString, setHeadComponents }) => {
+    const sheet = new ServerStyleSheet()
+    const html = renderToString(sheet.collectStyles(bodyComponent))
+    replaceBodyHTMLString(html);
+    setHeadComponents([sheet.getStyleElement()])
+};
